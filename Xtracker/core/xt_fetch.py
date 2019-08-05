8b555e38a0f3953720feed3d58a52b8531ca2a07
Abhishek Kumar <abkabhi>
2019-08-05 22:48:28 +0530

    added table output in console

--- Xtracker/core/xt_fetch.py
+++ Xtracker/core/xt_fetch.py
@@ -3,6 +3,7 @@ import os
 import sys
 import re
 from pprint import pprint
+from prettytable import PrettyTable
 
 sys.path.append(".")
 
@@ -57,6 +58,22 @@ class FetchTests:
             for item in v:
                 print(","+str(item[0])+","+item[1]+",")
                 
+    def print_test_table(self):
+        x = PrettyTable()
+        x.field_names = ["File","LineNumber", "TestFunction"]
+        x.align["LineNumber"] = "l"
+        x.align["TestFunction"] = "l"
+        x.align["File"] = "l"
+        for k,v in self.test_functions.items():
+#             print(k+",,,")
+            x.add_row([k[-35:],"",""])
+            for item in v:
+#                 print(","+str(item[0])+","+item[1]+",")
+                x.add_row(["",str(item[0]),item[1]])
+        print(x)
+
+        
+                
     def save_in_xl(self,xlPath):
         xl = XL_Man()
         xl.add_sheet("Test_functions")
@@ -94,7 +111,8 @@ if __name__=="__main__":
     FT = FetchTests("BDDTests","0*.py","/Users/abk/dev/python/ofc/bit/sb-automation-py-behave/BDDFramework/steps","step.*")
     FT.fetch_test_functions()
 #     FT.print_test_functions()
-    FT.save_in_xl("TestFunctions.xls")
+    FT.print_test_table()
+#     FT.save_in_xl("TestFunctions.xls")
     
 
     
